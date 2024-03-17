26259377ccf804ecd67e7e6202c8bcd39ad42fd1
Julia Tokola <jutokolak@gmail.com>
2024-03-17 13:00:15 +0200

    add frontend isAuthenticated functionality

--- blogbud/frontend/src/App.js
+++ blogbud/frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { AuthContext } from './context/authContext';
 import Layout from './components/Layout';
 import Reading from './components/blogById';
 import UserProfile from './components/userProfile';
@@ -11,20 +13,26 @@ import  Categories  from './components/categories';
 import BlogForm from './components/blogForm';
 
 function App() {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  console.log(isAuthenticated)
+
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/signup" element={<Signup/>} />
-        <Route path="/" element={<Layout />}>
-          <Route path="/mainpage" element={<Desktop />} />
-          <Route path="/blogs/:id" element={<Reading />} />
-          <Route path="/profile/:user_id" element={<UserProfile />} />
-          <Route path="/myprofile/" element={<MyProfile />} />
-          <Route path="/blogs/:id/update" element={<BlogUpdateForm />}/>
-          <Route path="/categories" element={<Categories />} /> 
-          <Route path="/blogform" element={<BlogForm />} />
-        </Route>
+        <Route path="/" element={ isAuthenticated ? <Layout /> : <Login /> } />
+        <Route path="/signup" element={<Signup/>} />
+        { isAuthenticated && (
+          <Route path="/" element={<Layout />}>
+            <Route path="/mainpage" element={<Desktop />} />
+            <Route path="/blogs/:id" element={<Reading />} />
+            <Route path="/profile/:user_id" element={<UserProfile />} />
+            <Route path="/myprofile/" element={<MyProfile />} />
+            <Route path="/blogs/:id/update" element={<BlogUpdateForm />}/>
+            <Route path="/categories" element={<Categories />} /> 
+            <Route path="/blogform" element={<BlogForm />} />
+          </Route>
+        )}
       </Routes>
     </BrowserRouter>
   );
